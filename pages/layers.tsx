719a316c04322d96be057e6daa31bac75cc9b5b2
Randy Howk <rhowk@estee.com>
2025-03-17 13:41:04 -0400

    Removed transitions

--- pages/layers.tsx
+++ pages/layers.tsx
@@ -1,18 +1,16 @@
-import { useContext, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { gsap } from 'gsap/dist/gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin';
 
 import { useIsomorphicLayoutEffect } from '../helpers/isomorphicEffect';
-import TransitionContext, { useTransitionContext } from '../context/TransitionContext'; // Import useTransitionContext
 
-gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
 export default function Layers() {
   const main = useRef(null);
   const scrollTween = useRef<gsap.core.Tween | null>(null); // Explicitly type scrollTween
   const [ctx] = useState(gsap.context(() => {}, main));
-  const { completed } = useTransitionContext(); // Use useTransitionContext
 
   const goToSection = (i) => {
     // Remove the GSAP instance with the specific ID
@@ -34,10 +32,10 @@ export default function Layers() {
   };
 
   useIsomorphicLayoutEffect(() => {
-    if (!completed) return;
     ctx.add(() => {
       const panels = gsap.utils.toArray('.panel') as HTMLElement[]; // Explicitly type panels
-      panels.forEach((panel: HTMLElement, i) => { // Explicitly type panel
+      panels.forEach((panel: HTMLElement, i) => {
+        // Explicitly type panel
         ScrollTrigger.create({
           trigger: panel,
           start: 'top bottom',
@@ -52,8 +50,9 @@ export default function Layers() {
         snap: 1 / (panels.length - 1),
       });
     });
+
     return () => ctx.revert();
-  }, [completed]);
+  }, []); // No dependency on `completed`
 
   return (
     <main ref={main}>
